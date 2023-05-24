5b81a04f3a3d66362a49395eef876a550929c1b3
Art12 <partizantest22@gmail.com>
2023-05-24 13:35:38 +0300

    test333

--- 124.py
+++ 124.py
@@ -1,4 +1,25 @@
-12312313
-12312344422
-44545544
+[33mcommit e78d7eac3acafb0f8211112045a2640092239273[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Art12 <partizantest22@gmail.com>
+Date:   Wed May 24 13:21:37 2023 +0300
 
+    firstchanges3str
+
+[1mdiff --git a/124.py b/124.py[m
+[1mindex e69de29..867852f 100644[m
+[1m--- a/124.py[m
+[1m+++ b/124.py[m
+[36m@@ -0,0 +1,4 @@[m
+[32m+[m[32m12312313[m
+[32m+[m[32m12312344422[m
+[32m+[m[32m44545544[m
+[32m+[m
+
+[33mcommit 581e22b4f415abc508660abc463077e73ddf07e9[m
+Author: Art12 <partizantest22@gmail.com>
+Date:   Wed May 24 12:42:35 2023 +0300
+
+    start testing
+
+[1mdiff --git a/124.py b/124.py[m
+[1mnew file mode 100644[m
+[1mindex 0000000..e69de29[m
